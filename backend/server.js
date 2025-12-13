a050f01cf86ceb9078e54238b549a95b0b07bd59
Kalsoom Asif <kalsoomasif.jokhio@drake.edu>
2025-12-13 12:12:19 -0600

    Allow Netlify frontend via CORS

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [process.env.CLIENT_ORIGIN, "http://localhost:5173"],
-    credentials: true
-  })
-);
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://cheery-cannoli-aaa4c9.netlify.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
